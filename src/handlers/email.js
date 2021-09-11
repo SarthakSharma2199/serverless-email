192b3fcf53d60f9b83e23e29aba268cd0a1ccea8
Sarthak Sharma <sarthaksharma2199@outlook.com>
2021-09-11 21:16:40 +0530

    sending email works

---
+++ src/handlers/email.js
@@ -0,0 +1,49 @@
+"use strict";
+
+require("dotenv").config({ path: "src/.env" });
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+module.exports.sendEmail = async function (context, req) {
+
+  let recipient;
+  let subject;
+  let text;
+
+  try {
+    
+    recipient = req.query.recipient || req.body.recipient;
+    subject = req.query.subject || req.body.subject;
+    text = req.query.text || req.body.text;
+
+    const msg = {
+      to: recipient,
+      from: process.env.VERIFIED_SENDER,
+      subject: subject,
+      text: text,
+      html: "<strong>" + text + "</strong>",
+    };
+
+    sgMail
+      .send(msg)
+      .then(() => {
+        context.res = {
+          body: "Email Sent Successfully",
+        };
+      })
+      .catch((error) => {
+        context.res = {
+          status: 400,
+          body: error,
+        };
+      });
+
+  } catch (error) {
+     context.res = {
+       status: 400,
+       body: "Invalid request body, please check again",
+     };
+  }
+  
+};
+
--- src/handlers/goodbye.js
+++ src/handlers/goodbye.js
@@ -6,7 +6,7 @@ module.exports.sayGoodbye = async function (context, req) {
   if (req.query.name || (req.body && req.body.name)) {
     context.res = {
       // status: 200, /* Defaults to 200 */
-      body: 'Goodbye ' + (req.query.name || req.body.name),
+      body: 'Goodbye: ' + (req.query.name || req.body.name),
     };
   } else {
     context.res = {
